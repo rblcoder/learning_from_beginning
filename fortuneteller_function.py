500b5a93f0656f828370b13f88d4fe27d76fc83a
Rupa Lahiri <35212670+rblcoder@users.noreply.github.com>
2020-08-13 22:16:19 +0530

    fortune teller and hellotkinter

---
+++ fortuneteller_function.py
@@ -0,0 +1,26 @@
+# https://automatetheboringstuff.com/2e/chapter3/
+# Fortune teller using a function
+
+import random
+
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It is certain'
+    elif answerNumber == 2:
+        return 'It is decidedly so'
+    elif answerNumber == 3:
+        return 'Yes'
+    elif answerNumber == 4:
+        return 'Reply hazy try again'
+    elif answerNumber == 5:
+        return 'Ask again later'
+    elif answerNumber == 6:
+        return 'Concentrate and ask again'
+    elif answerNumber == 7:
+        return 'My reply is no'
+    elif answerNumber == 8:
+        return 'Very doubtful'
+
+r = random.randint(1, 8)
+fortune = getAnswer(r)
+print(fortune)
---
+++ hellotkinter.py
@@ -0,0 +1,11 @@
+"""Hello Developer from Tkinter!"""
+
+
+from tkinter import *
+from tkinter.ttk import *
+
+root = Tk()
+root.geometry("800x600")
+label = Label(root, text="Welcome Python Developer!", font=("TkDefaultFont", 64), wraplength=600)
+label.pack()
+root.mainloop()
