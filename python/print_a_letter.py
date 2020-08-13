2ad1bf7ce2b8a58ae9388788a44e8ba01d167a7d
Rupa Lahiri <35212670+rblcoder@users.noreply.github.com>
2020-08-13 22:15:02 +0530

    Create print_a_letter.py

---
+++ python/print_a_letter.py
@@ -0,0 +1,21 @@
+# https://nostarch.com/pythonforkids
+# How to print a letter
+
+spaces = ' ' * 25
+print(f'{spaces} Wizard of Oz' )
+print(f'{spaces} Emerald City'  )
+print(f'{spaces} Land of Oz' )
+print()
+print()
+print('Dear Sir')
+print()
+print('Dorothy came to meet the Scarecrow, who wants a brain,')
+print('the Tin Woodman, who desires a heart, and the Cowardly Lion,')
+print('who needs courage.')
+print()
+print('The Tin Woodman threw away the tiles')
+print('from the rooftop.')
+print('Kindly fix the roof.')
+print()
+print('Regards,')
+print('Malcolm Dithering')
